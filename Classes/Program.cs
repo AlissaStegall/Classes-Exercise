0bebc7bb9aaab42dad139f07bc8544bc578ee5f7
AlissaStegall <alissaistegall@gmail.com>
2023-05-08 16:32:19 -0700

    created a class with properties/objects

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //option 1
+            var myCar1 = new Car("Hyundai", "Kona", 2022);
+
+            //option 2
+            var myCar2 = new Car();
+            myCar2.Make = "Honda";
+            myCar2.Model = "Civic";
+            myCar2.Year = 2021;
+
+            //option 3
+            var myCar3 = new Car()
+            {
+                Make = "Chevy",
+                Model = "Nova",
+                Year = 1974
+            };
+
+            var carList = new List<Car>() { myCar1, myCar2, myCar3 };
+
+            foreach(var ride in carList)
+            {
+                Console.WriteLine($"{ride.Make} {ride.Model} {ride.Year}");
+            }
         }
     }
 }
